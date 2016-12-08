797d860e0b0248abf01003f0cf981c3cc52e1c54
Klucinka007 <Palkovicova.lucia@gmail.com>
2016-12-08 19:48:05 +0100

    first commit

---
+++ BookTable/Models/Booking.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BookTable.Models
+{
+    public class Booking
+    {
+        public int BookingID { get; set; }
+        public int UserId { get; set; }
+        public int RestaurantId { get; set; }
+        public DateTime BookingDate { get; set; }
+        public int NumberOfPeople { get; set; }
+        public int SeatNumber { get; set; }
+        public virtual Restaurant Restaurant { get; set; }
+
+        public virtual ApplicationUser ApplicationUser { get; set; }
+    }
+}
---
+++ BookTable/Models/Restaurant.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BookTable.Models
+{
+    public class Restaurant
+    {
+
+        public int RestaurantID { get; set; }
+        public int Cvr { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public int Phone { get; set; }
+        public string Email { get; set; }
+        public string Description { get; set; }
+
+
+        public virtual ICollection<Booking> Bookings { get; set; }
+    }
+}
